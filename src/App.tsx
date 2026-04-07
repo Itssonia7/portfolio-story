8ef3b05179d01ba75d755ceb8c842e63b987a357
Sonia Nanwani <soniananvani7@gmail.com>
2026-04-07 16:05:44 +0530

    Fix Tailwind v4 setup using Vite plugin

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 function App() {
   return (
-    <div className="h-screen flex items-center justify-center bg-black">
-      <h1 className="text-4xl text-green-400">Tailwind Working 🚀</h1>
+    <div className="bg-black text-white">
+
+      {/* Chapter 1 */}
+      <section className="h-screen flex items-center justify-center">
+        <h1 className="text-4xl md:text-6xl font-bold text-center">
+          Hi, I’m Sonia 👋
+        </h1>
+      </section>
+
     </div>
   );
 }
